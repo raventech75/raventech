86edd8c160c23ffa70f2a5081db6fe04fb4bef9f
Feridun <feridun@macbookpro-1.home>
2025-09-03 21:29:07 +0200

    Production-V4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import EditorLayout from '@/components/EditorLayout';
+import { Analytics } from "@vercel/analytics/next"
 
 export default async function Page() {
   const supabase = createServerComponentClient({ cookies });
