221cec18f1daca63d0b0bfc3e5c73dd1e1ad61d1
Feridun <feridun@macbookpro-1.home>
2025-09-03 17:57:39 +0200

    Production-V2

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export const config = {
-  matcher: ['/', '/editor', '/projects/:path*'], // on remet "/" explicitement
+  matcher: ['/sign-in', '/', '/projects/:path*'], // on remet "/" explicitement
 };
 
 export function middleware(req: NextRequest) {
